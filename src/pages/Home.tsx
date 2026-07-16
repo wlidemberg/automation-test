1af11977a7689610b514ccd45153e5bc8b410374
Wlidemberg <automatizei@gmail.com>
2026-07-16 17:20:51 -0300

    fix: adiciona rolagem automatica para hashes ao navegar entre paginas e garante ids nas secoes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Layout from '../components/Layout'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
@@ -6,6 +8,24 @@ import PortfolioSection from '../components/PortfolioSection'
 import CtaSection from '../components/CtaSection'
 
 export default function Home() {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace('#', '')
+      const element = document.getElementById(id)
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' })
+          // Clean the hash from the browser URL to keep it friendly and clean
+          window.history.replaceState(null, '', window.location.pathname)
+        }, 100)
+      }
+    } else {
+      window.scrollTo(0, 0)
+    }
+  }, [location])
+
   return (
     <Layout>
       {/* Hero Entrance Section */}
